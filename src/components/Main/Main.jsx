348656a24d533cbe5035a678e0d7fb38577d4add
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-11-13 12:28:21 +0530

    made some changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../Context/Context";
 
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -10,53 +21,78 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello , I'm CloneAI</span>
-          </p>
-          <p>How can i assist you today ?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>
-              Is useEffect dependency array specified accurately to avoid
-              unnecessary re-renders?
-            </p>
-            <img src={assets.compass_icon} alt="" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello , I'm CloneAI</span>
+              </p>
+              <p>How can i assist you today ?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>
+                  Is useEffect dependency array specified accurately to avoid
+                  unnecessary re-renders?
+                </p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>
+                  Are useState and useEffect used optimally to manage state and
+                  side effects
+                </p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>
+                  Is useCallback used to memoize functions that are passed as
+                  props?
+                </p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>
+                  Is useReducer used in place of useState for complex state
+                  logic or when multiple state variables are interdependent?
+                </p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>
-              Are useState and useEffect used optimally to manage state and side
-              effects
-            </p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>
-              Is useCallback used to memoize functions that are passed as props?
-            </p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>
-              Is useReducer used in place of useState for complex state logic or
-              when multiple state variables are interdependent?
-            </p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
             <input
               type="text"
               placeholder="Enter a prompt here"
-              name=""
-              id=""
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
             />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
             </div>
           </div>
           <p className="bottom-info">
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,16 +1,43 @@
-import { createContext } from "react";
-import run from "../config/cloneAI";
+import { createContext, useState } from "react";
+import run from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-  const onSend = async (prompt) => {
-    await run(prompt);
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [previousPrompt, setPreviousPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input)
+    const response = await run(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
   };
 
-  onSend("What is reactJS");
+  const contextValue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    previousPrompt,
+    setPreviousPrompt,
+    showResult,
+    setShowResult,
+    loading,
+    setLoading,
+    resultData,
+    setResultData,
+    onSent,
+  };
 
-  const contextValue = {};
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
